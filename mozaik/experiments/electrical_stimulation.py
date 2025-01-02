12a177fd331aa4b5ff0cc406862b60a8bf904c7b
lucabaroni <loocabaroni@gmail.com>
2025-01-02 15:40:15 +0100

    fix black

--- mozaik/experiments/electrical_stimulation.py
+++ mozaik/experiments/electrical_stimulation.py
@@ -12,9 +12,9 @@ import mozaik.stimuli.vision.topographica_based as topo
 
 class RandomSingleNeuronStepCurrentInjection(Experiment):
     """
-    This experiment schedules many single neuron injections, whereby neurons are randomly picked from the set defined by 
+    This experiment schedules many single neuron injections, whereby neurons are randomly picked from the set defined by
     population selector provided in stimulation_configuration.
-    
+
     Parameters
     ----------
     duration : float (ms)
@@ -22,16 +22,16 @@ class RandomSingleNeuronStepCurrentInjection(Experiment):
 
     current : float (mA)
           The magnitude on injected current
-          
+
     sheet : str
           The sheet from which the neurons are selected
-    
+
     num_neurons : int
           The number of neurons that will be stimulated
 
     num_trials : int
           Number of trials to repeat each repetition (the order of stimulations will be randomised)
-    
+
     stimulation_configuration : ParameterSet
           The population selector from which the neurons are randomly selected
 
@@ -40,67 +40,79 @@ class RandomSingleNeuronStepCurrentInjection(Experiment):
 
     """
 
+    required_parameters = ParameterSet(
+        {
+            "duration": float,
+            "current": float,
+            "sheet": str,
+            "num_neurons": int,  # initialize afferent layer 4 projections
+            "num_trials": int,
+            "stimulation_configuration": ParameterSet,
+            "experiment_random_seed": int,
+        }
+    )
 
-    required_parameters = ParameterSet({
-            'duration': float,
-            'current' : float,
-            'sheet' : str,
-            'num_neurons' : int,           # initialize afferent layer 4 projections
+    def __init__(self, model, parameters):
+        Experiment.__init__(self, model, parameters)
+        from mozaik.sheets.direct_stimulator import Depolarization
 
-            'num_trials' : int, 
-            'stimulation_configuration' : ParameterSet,
-            'experiment_random_seed' : int 
-       })
+        rng = np.random.default_rng(self.parameters.experiment_random_seed)
 
-    
-    def __init__(self,model,parameters):
-            Experiment.__init__(self, model,parameters)
-            from mozaik.sheets.direct_stimulator import Depolarization
+        population_selector = load_component(
+            self.parameters.stimulation_configuration.component
+        )
+        ids = population_selector(
+            model.sheets[self.parameters.sheet],
+            self.parameters.stimulation_configuration.params,
+        ).generate_idd_list_of_neurons()
 
-            rng = np.random.default_rng(self.parameters.experiment_random_seed)
+        ids = rng.choice(ids, size=self.parameters.num_neurons, replace=False)
 
-            population_selector = load_component(self.parameters.stimulation_configuration.component)
-            ids = population_selector(model.sheets[self.parameters.sheet],self.parameters.stimulation_configuration.params).generate_idd_list_of_neurons()
+        self.direct_stimulation = []
 
-            ids = rng.choice(ids,size=self.parameters.num_neurons,replace=False)
+        for i in range(0, self.parameters.num_trials):
+            rng.shuffle(ids)
+            for idd in ids:
 
-            self.direct_stimulation = []
+                p = MozaikExtendedParameterSet(
+                    {
+                        "current": self.parameters.current,
+                        "population_selector": MozaikExtendedParameterSet(
+                            {
+                                "component": "mozaik.sheets.population_selector.IDList",
+                                "params": MozaikExtendedParameterSet(
+                                    {"list_of_ids": [idd]}
+                                ),
+                            }
+                        ),
+                    }
+                )
 
-            for i in range(0,self.parameters.num_trials):
-                rng.shuffle(ids)
-                for idd in ids:
+                d = OrderedDict()
 
-                    p = MozaikExtendedParameterSet({
-                                                    'current' : self.parameters.current,
-                                                    'population_selector' : MozaikExtendedParameterSet({
-                                                         'component' : 'mozaik.sheets.population_selector.IDList',
-                                                         'params' : MozaikExtendedParameterSet({
-                                                            'list_of_ids' : [idd]
-                                                          })
-                                                      })
-                                                  })
+                d[self.parameters.sheet] = [
+                    Depolarization(model.sheets[self.parameters.sheet], p)
+                ]
 
-                    d  = OrderedDict()
-
-                    d[self.parameters.sheet] = [Depolarization(model.sheets[self.parameters.sheet],p)]
-                    
-                    self.direct_stimulation.append(d)
-                    self.stimuli.append(
-                                InternalStimulus(   
-                                                    frame_duration=self.parameters.duration, 
-                                                    duration=self.parameters.duration,
-                                                    trial=i,
-                                                    direct_stimulation_name='Injection',
-                                                    direct_stimulation_parameters = p
-                                                )
-                                        )
+                self.direct_stimulation.append(d)
+                self.stimuli.append(
+                    InternalStimulus(
+                        frame_duration=self.parameters.duration,
+                        duration=self.parameters.duration,
+                        trial=i,
+                        direct_stimulation_name="Injection",
+                        direct_stimulation_parameters=p,
+                    )
+                )
 
 
-class RandomSingleNeuronStepCurrentInjectionDuringDriftingSinusoidalGratingStimulation(VisualExperiment):
+class RandomSingleNeuronStepCurrentInjectionDuringDriftingSinusoidalGratingStimulation(
+    VisualExperiment
+):
     """
-    This experiment schedules many single neuron injections, whereby neurons are randomly picked from the set defined by 
+    This experiment schedules many single neuron injections, whereby neurons are randomly picked from the set defined by
     population selector provided in stimulation_configuration.
-    
+
     Parameters
     ----------
     duration : float (ms)
@@ -108,10 +120,10 @@ class RandomSingleNeuronStepCurrentInjectionDuringDriftingSinusoidalGratingStimu
 
     current : float (mA)
           The magnitude on injected current
-          
+
     sheet : str
           The sheet from which the neurons are selected
-    
+
     num_neurons : int
           The number of neurons that will be stimulated
 
@@ -120,16 +132,16 @@ class RandomSingleNeuronStepCurrentInjectionDuringDriftingSinusoidalGratingStimu
 
     grating_spatial_frequency : float
                       Spatial frequency of the grating.
-                      
+
     grating_temporal_frequency : float
                       Temporal frequency of the grating.
-    
-    grating_contrasts : list 
+
+    grating_contrasts : list
               List of contrasts (expressed as % : 0-100%) of the grating.
 
     grating_num_orientations : int
               Number of evenly spaced orientations to show.
-              
+
     stimulation_configuration : ParameterSet
           The population selector from which the neurons are randomly selected
 
@@ -137,72 +149,88 @@ class RandomSingleNeuronStepCurrentInjectionDuringDriftingSinusoidalGratingStimu
           Random seed of the experiment
     """
 
-
-    required_parameters = ParameterSet({
-            'duration': float,
-            'current' : float,
-            'sheet' : str,
-            'num_neurons' : int,
-            'num_trials' : int, 
-            'grating_spatial_frequency' : float,
-            'grating_temporal_frequency' : float,
-            'grating_contrasts' : list,
-            'grating_num_orientations' : int,
-            'stimulation_configuration' : ParameterSet,
-            'experiment_random_seed' : int 
-        })
+    required_parameters = ParameterSet(
+        {
+            "duration": float,
+            "current": float,
+            "sheet": str,
+            "num_neurons": int,
+            "num_trials": int,
+            "grating_spatial_frequency": float,
+            "grating_temporal_frequency": float,
+            "grating_contrasts": list,
+            "grating_num_orientations": int,
+            "stimulation_configuration": ParameterSet,
+            "experiment_random_seed": int,
+        }
+    )
 
     def generate_stimuli(self):
-    
-            from mozaik.sheets.direct_stimulator import Depolarization
 
-            rng = np.random.default_rng(self.parameters.experiment_random_seed)
+        from mozaik.sheets.direct_stimulator import Depolarization
 
-            population_selector = load_component(self.parameters.stimulation_configuration.component)
-            ids = population_selector(self.model.sheets[self.parameters.sheet],self.parameters.stimulation_configuration.params).generate_idd_list_of_neurons()
+        rng = np.random.default_rng(self.parameters.experiment_random_seed)
 
-            ids = rng.choice(ids,size=self.parameters.num_neurons,replace=False)
+        population_selector = load_component(
+            self.parameters.stimulation_configuration.component
+        )
+        ids = population_selector(
+            self.model.sheets[self.parameters.sheet],
+            self.parameters.stimulation_configuration.params,
+        ).generate_idd_list_of_neurons()
 
-            self.direct_stimulation = []
+        ids = rng.choice(ids, size=self.parameters.num_neurons, replace=False)
 
-            for c in self.parameters.grating_contrasts:
-                  for i in range(0, self.parameters.grating_num_orientations):
-                        for t in range(0,self.parameters.num_trials):
-                              rng.shuffle(ids)
-                              for idd in ids:
+        self.direct_stimulation = []
 
-                                    p = MozaikExtendedParameterSet({
-                                                                  'current' : self.parameters.current,
-                                                                  'population_selector' : MozaikExtendedParameterSet({
-                                                                        'component' : 'mozaik.sheets.population_selector.IDList',
-                                                                        'params' : MozaikExtendedParameterSet({
-                                                                              'list_of_ids' : [idd]
-                                                                        })
-                                                                        })
-                                                                  })
+        for c in self.parameters.grating_contrasts:
+            for i in range(0, self.parameters.grating_num_orientations):
+                for t in range(0, self.parameters.num_trials):
+                    rng.shuffle(ids)
+                    for idd in ids:
 
-                                    d  = OrderedDict()
+                        p = MozaikExtendedParameterSet(
+                            {
+                                "current": self.parameters.current,
+                                "population_selector": MozaikExtendedParameterSet(
+                                    {
+                                        "component": "mozaik.sheets.population_selector.IDList",
+                                        "params": MozaikExtendedParameterSet(
+                                            {"list_of_ids": [idd]}
+                                        ),
+                                    }
+                                ),
+                            }
+                        )
 
-                                    d[self.parameters.sheet] = [Depolarization(self.model.sheets[self.parameters.sheet],p)]
-                                    
-                                    self.direct_stimulation.append(d)
-                                    self.stimuli.append(topo.FullfieldDriftingSinusoidalGrating(
-                                                      frame_duration = self.frame_duration,
-                                                      size_x=self.model.visual_field.size_x,
-                                                      size_y=self.model.visual_field.size_y,
-                                                      location_x=0.0,
-                                                      location_y=0.0,
-                                                      background_luminance=self.background_luminance,
-                                                      contrast = c,
-                                                      duration=self.parameters.duration,
-                                                      density=self.density,
-                                                      trial=t,
-                                                      orientation=np.pi/self.parameters.grating_num_orientations*i,
-                                                      spatial_frequency=self.parameters.grating_spatial_frequency,
-                                                      temporal_frequency=self.parameters.grating_temporal_frequency,
-                                                      direct_stimulation_name='Injection',
-                                                      direct_stimulation_parameters = p
-                                                ))
+                        d = OrderedDict()
+
+                        d[self.parameters.sheet] = [
+                            Depolarization(self.model.sheets[self.parameters.sheet], p)
+                        ]
+
+                        self.direct_stimulation.append(d)
+                        self.stimuli.append(
+                            topo.FullfieldDriftingSinusoidalGrating(
+                                frame_duration=self.frame_duration,
+                                size_x=self.model.visual_field.size_x,
+                                size_y=self.model.visual_field.size_y,
+                                location_x=0.0,
+                                location_y=0.0,
+                                background_luminance=self.background_luminance,
+                                contrast=c,
+                                duration=self.parameters.duration,
+                                density=self.density,
+                                trial=t,
+                                orientation=np.pi
+                                / self.parameters.grating_num_orientations
+                                * i,
+                                spatial_frequency=self.parameters.grating_spatial_frequency,
+                                temporal_frequency=self.parameters.grating_temporal_frequency,
+                                direct_stimulation_name="Injection",
+                                direct_stimulation_parameters=p,
+                            )
+                        )
 
     def do_analysis(self, data_store):
         pass
