82bc2426657e864f02757988af53c594ad8918c4
goirik-chakrabarty <chakrabarty.goirik@gmail.com>
2026-01-28 15:47:35 +0100

    Change sbatch parameters

--- apptainer-runners/mozaik-simulation.sh
+++ apptainer-runners/mozaik-simulation.sh
@@ -1,8 +1,8 @@
 cd /project
-rm -r SelfSustainedPushPull_test:test8_____
+rm -r SelfSustainedPushPull_test:testnasks32_____
 
 # python run_parameter_search.py run_spont.py nest param_MSA/defaults 
 # python run_spont.py nest 2 param_MSA/defaults 'test'
 # echo "Waiting for debugger attach..."
 # python -m debugpy --listen 0.0.0.0:5678 --wait-for-client run.py nest 2 param_MSA/defaults 'test:3'
-python -u run.py nest 8 param_MSA/defaults 'test:test8'
+python -u run.py nest 32 param_MSA/defaults 'test:testnasks32'
